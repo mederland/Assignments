85a400bef493cd6981ba813189261140a524e70b
mederland <mederland@gmail.com>
2022-06-29 15:51:19 -0400

    Initial Commit

---
+++ Shared/FirstlessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstlessonApp.swift
+//  Shared
+//
+//  Created by Consultant on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstlessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
